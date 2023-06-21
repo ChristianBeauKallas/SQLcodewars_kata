29968384ca9ff0595767a2fad1a31ac9c9fdb977
Christian Kallas <bkallas3@gmail.com>
2023-06-21 07:47:43 -0700

    completed codewars_kata

---
+++ SQLKata.sql
@@ -0,0 +1,11 @@
+SELECT name, age FROM users WHERE age >= 18;
+
+SELECT (price * amount) AS total FROM items;
+
+Select -number AS res FROM opposite;
+
+SELECT * FROM companies ORDER BY employees desc;
+
+SELECT * FROM students WHERE tuition_received = false;
+
+SELECT * FROM travelers WHERE NOT country = 'Canada' AND NOT country = 'Mexico' AND NOT country = 'USA';
